c510092b291e93506e5b3ab2ad97379c495bd4cd
Deykel17 <161171059+Deykel17@users.noreply.github.com>
2025-03-26 20:09:21 -0600

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit_antd_components as sac
 from streamlit_option_menu import option_menu
 
-# Import other modules
+
 import about
 from eda import EDAApp
 import results
@@ -12,26 +12,26 @@ main_color = "#384B70"
 # Main App
 with st.sidebar:
     selected = option_menu(
-        menu_title="Menu",
-        options=["EDA", "Results", "About"],
-        icons=["bar-chart", "rocket-takeoff", "bi bi-mortarboard"],
-        menu_icon="cast",
-        default_index=0,
-        styles={
+        menu_title="Menu",  
+        options=["EDA", "Results", "About"],  
+        icons=["bar-chart", "rocket-takeoff", "bi bi-mortarboard"],  
+        menu_icon="cast",  
+        default_index=0, 
+        styles={  
             "container": {"background-color": "#f8f9fa"},
             "icon": {"font-size": "18px"}, 
             "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px", "--hover-color": "#d9d9d9"},
             "nav-link-selected": {"background-color": main_color, "color": "white", "icon": {"color": "white"} },
         }
     )
-  
+
+
 if selected == "EDA":
     EDAApp.show()
 
 elif selected == "Results":
     results.show()
-    
+
 elif selected == "About":
     about.show()
-
 
